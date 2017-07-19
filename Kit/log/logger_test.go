d28f3cd362c9b218cad023798099eec4c445440d
Sudherson Jeyakumar <sudhersonjeyakumar@Sudhersons-MacBook-Pro.local>
2017-07-18 20:25:24 -0400

    Added config

--- Kit/log/logger_test.go
+++ Kit/log/logger_test.go
@@ -6,6 +6,8 @@ import (
 	"strings"
 	"testing"
 
+	"io/ioutil"
+
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -51,5 +53,60 @@ func TestBaseLogger_ChangePrefix(t *testing.T) {
 
 		})
 
+		Convey("When common prefix is changed, content should match", func() {
+			s.Prefix("common prefix changed")
+			s.Infof("%s", "Writing third String")
+			So(strings.TrimRight(w.String(), "\n"), ShouldEndWith, "INFO <common prefix changed> : Writing third String")
+
+			s.Errorf("%s", "Writing fourth String")
+			So(strings.TrimRight(w.String(), "\n"), ShouldEndWith, "ERROR <common prefix changed> : Writing fourth String")
+
+		})
+	})
+}
+
+func TestBaseLogger_Enabler(t *testing.T) {
+	var w bytes.Buffer
+	Convey("Given, With Buffer as writer", t, func() {
+		s, err := InitBaseLogger(&w, "hello")
+		So(err, ShouldBeNil)
+
+		s.Enabler(false)
+
+		s.Infof("%s", "Writing first String")
+		So(strings.TrimRight(w.String(), "\n"), ShouldEqual, "")
+	})
+}
+
+func TestFileLogger_ChangePrefix(t *testing.T) {
+
+	f, err := os.OpenFile("./test/log.txt", os.O_RDWR|os.O_CREATE, 0777)
+	if err != nil {
+		return
+	}
+	defer os.Remove("./test/log.txt")
+
+	Convey("Given, With File as writer", t, func() {
+		Convey("Given that a file is prepared", func() {
+			s, err := InitFileLogger(f, "hello")
+			So(err, ShouldBeNil)
+
+			s.Infof("%s", "Writing first String")
+
+			Convey("When prefix is changed, content should match", func() {
+				s.PrefixLogType("info", "changed")
+				s.Infof("%s", "Writing second String")
+				s.Close()
+
+				allContent, err := ioutil.ReadFile("./test/log.txt")
+				So(err, ShouldBeNil)
+				So(strings.Trim(string(allContent), "\n"), ShouldEndWith, "INFO <changed> : Writing second String")
+			})
+		})
+
+		Convey("When an empty writer is given as input", func() {
+			_, err := InitFileLogger(nil, "hello")
+			So(err, ShouldNotBeNil)
+		})
 	})
 }
